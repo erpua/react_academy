477b25e30f63d925ad274f4f7ad948b7b3c921f4
intn <ievgen.tovtin@gft.com>
2022-04-25 19:03:10 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Logo from './components/Logo';
+import PaintingList from './components/PaintingList';
+import Panel from './components/Panel';
+import paintings from './paintings.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <Panel title="The fresh news">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam,
+          obcaecati dolorum assumenda vitae aspernatur, aliquid numquam
+          explicabo, facere tenetur unde dolorem quo! Sit iusto natus at,
+          aliquam, repellendus repellat ipsa eligendi dolorem tempore atque
+          reprehenderit nulla magnam reiciendis, aliquid minus tenetur ipsam
+          fuga. Quas vel, sunt voluptatum debitis incidunt numquam?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <a href="https://google.com">Read more...</a>
+      </Panel>
+
+      <Panel>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+          explicabo aperiam architecto perspiciatis quae amet. Porro magni
+          laudantium aspernatur debitis deserunt ipsam. Nostrum id accusamus
+          praesentium eum incidunt tenetur cum!
+        </p>
+      </Panel>
+
+      <Logo text="the main app component-container" />
+      <PaintingList paintings={paintings} />
     </div>
   );
 }
