23ecca66b28a5d93228019f7a4ae30a2a9315ab8
intn <zhe.tovtin@gmail.com>
2022-05-03 16:06:36 -0500

    brnahces configured

--- src/App.js
+++ src/App.js
@@ -2,51 +2,22 @@
 import React from 'react';
 
 //Components
-import PaintingList from './components/PaintingList/PaintingList';
-import Panel from './components/Panel/Panel';
-import ColorPicker from './components/ColorPicker/ColorPicker';
-import Notification from './components/Notification/Notification';
+import Counter from './components/Counter/Counter';
 
-//JSON
-import paintings from './paintings.json';
-
-const colorPickerOptions = [
+/* const colorPickerOptions = [
   { label: 'red', color: '#F44336' },
   { label: 'green', color: '#4CAF50' },
   { label: 'blue', color: '#2196F3' },
   { label: 'grey', color: '#607D8B' },
   { label: 'pink', color: '#E91E63' },
   { label: 'indigo', color: '#3F51B5' },
-];
+]; */
 
 // <></> => Babel converts to <React.Fragment> < /React.Fragment>
 const App = () => {
   return (
     <>
-      <ColorPicker options={colorPickerOptions} />
-
-      <Notification
-        text="Notification component SUCCESS"
-        type="success"
-      />
-
-      <Notification
-        text="Notification component ERROR"
-        type="error"
-      />
-
-      <Panel title="The fresh news">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit.
-        </p>
-
-        <a href="https://google.com">Read more...</a>
-      </Panel>
-
-      <Panel>
-        <PaintingList paintings={paintings} />
-      </Panel>
+      <Counter text="This is branch 03 - Counter.js Component"></Counter>
     </>
   );
 };
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Counter = ({ text }) => {
+  return (
+    <div>
+      <h1>This is Counter component</h1>
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default Counter;
