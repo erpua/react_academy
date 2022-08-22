7738d1a24dd16d450cd634d016c2a314bf0ecd71
intn <zhe.tovtin@gmail.com>
2022-08-22 15:24:20 -0500

    feature(12-selectors)

--- src/components/TodoFilter/TodoFilter.js
+++ src/components/TodoFilter/TodoFilter.js
@@ -4,9 +4,6 @@ import { connect } from 'react-redux';
 
 import { todosSelectors, changeFilter } from '../../redux/todos';
 
-/* import * as todosActions from '../../redux/todos/todos-actions';
-import todosSelectors from '../../redux/todos/todos-selectors'; */
-
 const Filter = ({ value, onChange }) => (
   <div className="TodoFilter">
     <p className="TodoFilter__label">Filter by content</p>
