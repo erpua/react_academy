32ee9f57ec86dbe735d3e0a7adc88dbc376c1073
intn <zhe.tovtin@gmail.com>
2022-09-28 14:31:29 -0500

    feature(16-redux-react-hooks):added new feature

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
+import store from './redux.store';
 import 'modern-normalize/modern-normalize.css';
 import './styles/base.scss';
-import AuthProvider from './context/AuthProvider';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-/* import authContext from './context/authContext';
-console.log('authContext =>', authContext); */
-
-// root.render(
-//  /*  <React.StrictMode> */
-//     <BrowserRouter>
-//     {/* Context automatically provide context with variable "value={{ a:5, b:10 }}" */}
-//       <authContext.Provider value={{ name: 'Mango' }}>
-//         {/* Everything that is inside of App => has automatic acces to value of object "Provider" */}
-//         <App />
-//       </authContext.Provider>
-//     </BrowserRouter>
-// /*   </React.StrictMode> */
-// );
-
 root.render(
-      <BrowserRouter>
-        <AuthProvider>
+  <React.StrictMode>
+    <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
+        <BrowserRouter>
           <App />
-        </AuthProvider>
-      </BrowserRouter>
-
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
   );
