134b416f3084f81c528030fc7cfd10389d58320d
intn <zhe.tovtin@gmail.com>
2022-10-03 13:43:20 -0500

    refactor(16-router-n-react-hooks):converted AppBar to Hooks

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,4 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Navigation from './Navigation';
+import UserMenu from './UserMenu';
+import AuthNav from './AuthNav';
+import { authSelectors } from '../redux/auth';
+
+const styles = {
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottom: '1px solid #2A363B',
+  },
+};
+
+export default function AppBar(){
+  const isLoggedIn = useSelector(authSelectors.getIsAuthenticated);
+
+  return (
+      <header style={styles.header}>
+        <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+  </header>
+  )
+};
+
+
+/* import React from 'react';
 import { connect } from 'react-redux';
 import Navigation from './Navigation';
 import UserMenu from './UserMenu';
@@ -25,7 +53,7 @@ const mapStateToProps = state => ({
   isAuthenticated: authSelectors.getIsAuthenticated(state),
 });
 
-export default connect(mapStateToProps)(AppBar);
+export default connect(mapStateToProps)(AppBar); */
 
 /* import React from 'react';
 import Navigation from './Navigation';
