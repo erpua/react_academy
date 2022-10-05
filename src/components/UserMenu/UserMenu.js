fe4502a8637f8ac6003fe3150ec4e4b2e3ef7b85
intn <zhe.tovtin@gmail.com>
2022-10-04 19:58:49 -0500

    refactor(16-router-n-react-hooks):component UserMenu && component TodoFilter converted to hooks and added useCallback hool

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -27,10 +27,50 @@ const UserMenu = ({ avatar, name, onLogout }) => (
 export default UserMenu;
  */
 
-import React from 'react';
+/* import React from 'react'; */
 /* import { connect } from 'react-redux'; */
 /* import { authSelectors, authOperations } from '../../redux/auth'; */
 /* import defaultAvatar from './default-avatar.png'; */
+
+// const styles = {
+//   container: {
+//     display: 'flex',
+//     alignItems: 'center',
+//   },
+//   avatar: {
+//     marginRight: 4,
+//   },
+//   name: {
+//     fontWeight: 700,
+//     marginRight: 12,
+//   },
+// };
+
+// const UserMenu = ({ avatar, name, onLogout }) => (
+//   <div style={styles.container}>
+//     <img src={avatar} alt="" width="32" style={styles.avatar} />
+//     <span style={styles.name}>Welcome, {name}</span>
+//     <button type="button" onClick={onLogout}>
+//       Logout
+//     </button>
+//   </div>
+// );
+
+// /* const mapStateToProps = state => ({
+//   name: authSelectors.getUsername(state),
+//   avatar: defaultAvatar,
+// });
+
+// const mapDispatchToProps = {
+//   onLogout: authOperations.logOut,
+// };
+//  */
+// export default UserMenu;
+
+import React from "react";
+import { useSelector, useDispatch, useCallback } from "react-redux";
+import { authSelectors, authOperations } from '../../redux/auth';
+import defaultAvatar from './default-avatar.png';
 
 const styles = {
   container: {
@@ -45,24 +85,41 @@ const styles = {
     marginRight: 12,
   },
 };
+  
+export default function UserMenu( ) {
+  const name = useSelector(authSelectors.getUsername);
 
-const UserMenu = ({ avatar, name, onLogout }) => (
-  <div style={styles.container}>
-    <img src={avatar} alt="" width="32" style={styles.avatar} />
-    <span style={styles.name}>Welcome, {name}</span>
-    <button type="button" onClick={onLogout}>
-      Logout
-    </button>
-  </div>
-);
+  //useDispatch !all the time returns reference for that same function. never changes
+  const dispatch = useDispatch();
 
-/* const mapStateToProps = state => ({
-  name: authSelectors.getUsername(state),
-  avatar: defaultAvatar,
-});
+  //wrong => every change of state will rerender the button and referrence will be different each time
+  //must use "hook for callbacks memoization"
+  //const onLogout = () => dispatch(authOperations.logOut());
+/* 
+  const memoizedCallback = useCallback(
+    () => {
+      doSomething(a, b);
+    },
+    [a, b],
+  ); */
 
-const mapDispatchToProps = {
-  onLogout: authOperations.logOut,
+  const onLogout = useCallback(
+    () => { 
+      dispatch(authOperations.logOut())
+    }, 
+    [dispatch]);
+
+
+  return (
+    <div style={styles.container}>
+      <img src={defaultAvatar} alt="" width="32" style={styles.avatar} />
+      <span style={styles.name}>Welcome, {name}</span>
+     {/*  <button type="button" onClick={()=> authOperations.logOut()}>
+        Logout
+      </button> */}
+      <button type="button" onClick={onLogout}>
+        Logout
+      </button>
+    </div>
+  );
 };
- */
-export default UserMenu;
