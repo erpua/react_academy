6fb505f07b9757312d4f28b804aeae15d093db63
intn <zhe.tovtin@gmail.com>
2022-10-13 13:44:35 -0500

    refactor(16-react-router-hook):terminated react courses

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,37 +1,65 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { authSelectors } from '../redux/auth';
+
+export default function PrivateRoute({
+  isAuthenticated,
+  redirectTo,
+  children,
+  ...routeProps
+}) {
+  const isLoggedIn = useSelector(authSelectors.getIsAuthenticated);
+
+  return (
+      <Route {...routeProps}>
+          {isLoggedIn ? children : <Redirect to={redirectTo} />}
+      </Route>
+  );
+}
+
+// const mapStateToProps = state => ({
+  //   isAuthenticated: authSelectors.getIsAuthenticated(state),
+  // });
+  
+  // export default connect(mapStateToProps)(PrivateRoute);
+//---------
+
+
+/* import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { authSelectors } from '../redux/auth'; */
 
 /**
  * - If route is private and user logged in => render of component
  * - Else render Redirect to /login
  */
-const PrivateRoute = ({
-  component: Component,
-  isAuthenticated,
-  redirectTo,
-  ...routeProps
-}) => (
-  <Route
+// const PrivateRoute = ({
+//   component: Component,
+//   isAuthenticated,
+//   redirectTo,
+//   ...routeProps
+// }) => (
+//   <Route
     /* {...routeProps}
     render={props =>
       isAuthenticated 
         ? <Component {...props} /> 
         : <Redirect to="/login" />
     } */
-    {...routeProps}
-    //if render () => props - props of router  => match, location, history
-    render={props =>
-      isAuthenticated 
-        ? <Component {...props} /> 
-        : <Redirect to={redirectTo} />
-    }
-  />
-);
+    // {...routeProps}
+    // if render () => props - props of router  => match, location, history
+//     render={props =>
+//       isAuthenticated 
+//         ? <Component {...props} /> 
+//         : <Redirect to={redirectTo} />
+//     }
+//   />
+// );
 
-const mapStateToProps = state => ({
-  isAuthenticated: authSelectors.getIsAuthenticated(state),
-});
+// const mapStateToProps = state => ({
+//   isAuthenticated: authSelectors.getIsAuthenticated(state),
+// });
 
-export default connect(mapStateToProps)(PrivateRoute);
+// export default connect(mapStateToProps)(PrivateRoute);
--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { connect } from 'react-redux';
 import { authSelectors } from '../redux/auth';
 
@@ -7,27 +8,34 @@ import { authSelectors } from '../redux/auth';
  * - If route is restricted => and user logged in => render of Redirect to /todos
  * - Else render of Component
  */
- 
-const PublicRoute = ({
-  component: Component,
-  isAuthenticated,
-  redirectTo,
-  ...routeProps
-}) => (
-  <Route
-    {...routeProps}
-    render={props =>
-      isAuthenticated && routeProps.restricted 
-        ? ( <Redirect to={ redirectTo } /> ) 
-        : (
-        <Component { ...props } />
-      )
-    }
-  />
-);
 
+/* 
 const mapStateToProps = state => ({
   isAuthenticated: authSelectors.getIsAuthenticated(state),
-});
+}); 
 
 export default connect(mapStateToProps)(PublicRoute);
+*/
+ 
+export default function PublicRoute({
+  isAuthenticated,
+  redirectTo,
+  children,
+  ...routeProps
+}) {
+  const inLoggedIn = useSelector(authSelectors.getIsAuthenticated);
+  return <Route {...routeProps}>{ 
+    inLoggedIn && routeProps.restricted 
+    ? (<Redirect to={redirectTo}/>) 
+    : (children)
+    }</Route>;
+}
+
+/* export default PublicRoute; */
+
+{/* render={props =>
+  isAuthenticated && routeProps.restricted 
+    ? ( <Redirect to={ redirectTo } /> ) 
+    : (
+    <Component { ...props } />
+  ) */}
